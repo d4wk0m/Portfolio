d15b8840e2762a73a69b26dfad32a8e958f7a328
d4wk0m <96721232+d4wk0m@users.noreply.github.com>
2022-03-16 11:56:12 +0100

    Website basics
    Added subsections, finished home page.

---
+++ js/home.js
@@ -0,0 +1,73 @@
+var navbarbutton = document.getElementsByClassName('navbar-button');
+
+for (i = 0; i < navbarbutton.length; i++) {   
+    navbarbutton[i].addEventListener("click", function() {
+        document.querySelectorAll('.navbar-active').forEach((item) => {
+            b = item.children[0].id;
+            item.classList.remove("navbar-active");
+            })
+        this.parentElement.classList.add("navbar-active");
+        show(this.id, b)
+    })
+}
+
+// document.querySelectorAll('.hidden').forEach((item) => {
+//     item.style.left = "100vw";
+//     })
+
+function show(a, b){
+    previousIndex = parseInt(document.getElementById(b).getAttribute( "order" ));
+    currentIndex = parseInt(document.getElementById(a).getAttribute( "order" ));
+
+    if (previousIndex > currentIndex){
+        // items go to right
+        document.getElementsByClassName(b)[0].style.left = "0";
+        document.querySelectorAll('.hidden').forEach((item) => {
+            item.style.left = "-100vw";
+            })
+            
+        document.getElementsByClassName(a)[0].classList.remove('hidden');
+        window.setTimeout(() => {
+            document.getElementsByClassName(b)[0].style.left = "100vw";
+            document.getElementsByClassName(a)[0].style.left = "0";
+            }, 1);
+        window.setTimeout(() => {
+            document.getElementsByClassName(b)[0].classList.remove('shown');
+            document.getElementsByClassName(a)[0].classList.add('shown');
+            },2);
+        contentheight(a);
+    }
+    else{
+        // items go to left
+        document.getElementsByClassName(b)[0].style.left = "0";
+        document.querySelectorAll('.hidden').forEach((item) => {
+            item.style.left = "100vw";
+            })
+            
+        document.getElementsByClassName(a)[0].classList.remove('hidden');
+        window.setTimeout(() => {
+            document.getElementsByClassName(a)[0].style.left = "0";
+            document.getElementsByClassName(b)[0].style.left = "-100vw";
+            }, 1);
+        
+        window.setTimeout(() => {
+            document.getElementsByClassName(b)[0].classList.remove('shown');
+            document.getElementsByClassName(a)[0].classList.add('shown');
+            },2);
+        contentheight(a);
+    }
+}
+
+function contentheight(a){
+    if (a === "home"){
+        
+    }
+    else{
+        var menuTop = document.getElementById('menu').offsetTop;
+        var menuHeight = document.getElementById('menu').offsetHeight;
+        gora = menuTop + menuHeight;
+        elheight = window.innerHeight - gora;
+        document.getElementsByClassName(a)[0].style.height = elheight + "px";
+        document.getElementsByClassName(a)[0].style.top = gora + "px";
+    }
+}
